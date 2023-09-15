3c7b4f24284a4e4aca4003125cc6f2368d9ce42b
A Mohudum Kamil <mohudumkamil@gmail.com>
2023-09-15 21:20:24 +0530

    test script modified

--- tests/testscripts/ut_1.js
+++ tests/testscripts/ut_1.js
@@ -4,47 +4,43 @@ const {By, Key, Builder} = require("selenium-webdriver");
 var webdriver = require('selenium-webdriver');
 var chrome = require("selenium-webdriver/chrome");
 
-const chromeProfilePath = "C:\\Users\\MKamil.Kamil_laptop\\AppData\\Local\\Google\\Chrome\\User Data";
+//const chromeProfilePath = "C:\\Users\\MKamil.Kamil_laptop\\AppData\\Local\\Google\\Chrome\\User Data";
+const chromeProfilePath = "C:\\tmp\\newdir";
 
 var options = new chrome.Options();
-options.addArguments(`--user-data-dir=${chromeProfilePath}`);
-
+options.addArguments(`user-data-dir=${chromeProfilePath}`);
+//options.addArguments("remote-debugging-port=9222");
 
 async function example(){
  
     //To wait for browser to build and launch properly
     try{
-      var driver = await new Builder().forBrowser("chrome").build();;//await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+      var driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
     }
     catch(e){
       console.log(e);
     }
 
-     //To fetch http://google.com from the browser with our code.
+    let srcUrl = "https://developer.chrome.com/docs/extensions/mv3/getstarted/";
+    await driver.get(srcUrl);
+    //To fetch div count from the react cs with our code.
+    let srcDivCount = await driver.findElement(By.className("logo__text")).getText();
+
+    let tempArr = srcDivCount.split('#');
+    srcDivCount = parseInt(tempArr[1]);
+     //To fetch dashboard.
      await driver.get("http://localhost/extensions/count-divs/dashboard.html");
 
-     //await driver.wait(until.elementLocated(By.name('url'), 10000));
-     //To send a search query by passing the value in searchString.
-     let url = await driver.findElement(By.name("url")).getText();
-     let divCount = await driver.findElement(By.name("count")).getText();
+     //To read the url and div count from the dashboard
+     let desUrl = await driver.findElement(By.name("url1")).getText();
+     let desDivCount = await driver.findElement(By.name("count1")).getText();
 
-    /* //Verify the page title and print it
-     var title = await driver.getTitle();
-     console.log('Title is:',title);*/
-
-     //It is always a safe practice to quit the browser after execution
-     //await driver.quit();
-
-     //To fetch http://google.com from the browser with our code.
-     await driver.get(url);
-     //To send a search query by passing the value in searchString.
-     let divCountSrc = 151;//await driver.findElement(By.name("count")).getText();
-
-
-     if(divCount == divCountSrc){
+     
+     if(srcDivCount === parseInt(desDivCount) && srcUrl == desUrl){
         console.log("Test Pass");
      }
-     await driver.quit;
+     //It is always a safe practice to quit the browser after execution
+     await driver.quit();
 
 }
 example();
